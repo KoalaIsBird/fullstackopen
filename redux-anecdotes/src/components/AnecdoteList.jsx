b3a28f98383b05f00a371afb23956d16c1a61b56
André Yurchenko <Andr>
2024-08-28 10:05:55 +0200

    exercise 6.9

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -4,9 +4,14 @@ import { voteAction } from '../reducers/anecdoteReducer'
 const AnecdoteList = () => {
   const dispatch = useDispatch()
 
-  const anecdotes = useSelector((state) =>
-    state.toSorted((n1, n2) => (n1.votes > n2.votes ? 0 : 1))
-  )
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    const sortFun = (n1, n2) => (n1.votes > n2.votes ? 0 : 1)
+    return filter === ''
+      ? anecdotes.toSorted(sortFun)
+      : anecdotes
+          .filter((a) => a.content.toLowerCase().includes(filter.toLowerCase()))
+          .toSorted(sortFun)
+  })
 
   const vote = (id) => {
     dispatch(voteAction(id))
