2c2950fd1c1f7d79a2034bbabd5a1ed9a47e7952
André <koalaisbird@gmail.com>
2025-01-09 19:47:05 +0100

    trying to clean up

--- blog/testing/tests/helpers.js
+++
@@ -1,9 +0,0 @@
-const likeBlog = async blog => {
-    await blog.getByRole('button', { name: 'like' }).click()
-}
-
-const getY = async blog => {
-    return (await blog.boundingBox()).y
-}
-
-module.exports = { likeBlog, getY }
